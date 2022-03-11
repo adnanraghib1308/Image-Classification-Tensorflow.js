775e48b4328b27fbe3a94a97ad98c60ce69efb93
Adnan Raghib <adnanraghib@192.168.1.8>
2022-03-11 11:34:36 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-console.log("hgjhjhg");
-const img = document.getElementById('img');
-const result = document.getElementById('result');
-const accuracy = document.getElementById('accuracy');
-const spinner = document.getElementById('spinner');
+console.log("sdhfljsh");
 
-const recognizeImage = () => {
-  // Load the model.
-  mobilenet.load().then(model => {
-    // Classify the image.
-    model.classify(img).then(predictions => {
-      const accuracyPercentage = parseFloat(predictions[0].probability * 100).toFixed(2);
-      result.innerHTML = "   " + predictions[0].className;
-      accuracy.innerHTML = "   " + accuracyPercentage + " %";
-      spinner.style.display = 'none';
-    });
-  });
+
+const main = () => {
+    const img = document.getElementById('img');
+    const result = document.getElementById('result');
+    const accuracy = document.getElementById('accuracy');
+    const spinner = document.getElementById('spinner');
+    const inpFile = document.getElementById('inpFile');
+
+    console.log(">>>>>", img);
+    const recognizeImage = () => {
+      // Load the model.
+      mobilenet.load().then(model => {
+        // Classify the image.
+        model.classify(img).then(predictions => {
+          const accuracyPercentage = parseFloat(predictions[0].probability * 100).toFixed(2);
+          result.innerHTML = "   " + predictions[0].className;
+          accuracy.innerHTML = "   " + accuracyPercentage + " %";
+          spinner.style.display = 'none';
+        });
+      });
+    }
+
+
+    inpFile.addEventListener("change", (e) => {
+        const file = e.target.files[0];
+        const imgUrl = window.URL.createObjectURL(file);
+
+        img.setAttribute('src', imgUrl);
+
+        spinner.style.display = "inline";
+        recognizeImage();
+    })
 }
 
-const inpFile = document.getElementById('inpFile');
-inpFile.addEventListener("change", (e) => {
-    const file = e.target.files[0];
-    const imgUrl = window.URL.createObjectURL(file);
-
-    img.setAttribute('src', imgUrl);
-    
-    spinner.style.display = "inline";
-    recognizeImage();
-})
+window.onload = () => {
+    main();
+}
