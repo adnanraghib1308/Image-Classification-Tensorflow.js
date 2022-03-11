e46a0c6a4301529a2e909fe3710a2a5f2e6cc092
Adnan Raghib <adnanraghib@192.168.1.8>
2022-03-11 10:40:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+  const img = document.getElementById('img');
+  const result = document.getElementById('result');
+  const accuracy = document.getElementById('accuracy');
+  const spinner = document.getElementById('spinner');
+
+  const recognizeImage = () => {
+    // Load the model.
+    mobilenet.load().then(model => {
+      // Classify the image.
+      model.classify(img).then(predictions => {
+        const accuracyPercentage = parseFloat(predictions[0].probability * 100).toFixed(2);
+        result.innerHTML = "   " + predictions[0].className;
+        accuracy.innerHTML = "   " + accuracyPercentage + " %";
+        spinner.style.display = 'none';
+      });
+    });
+  }
+
+  const inpFile = document.getElementById('inpFile');
+  inpFile.addEventListener("change", (e) => {
+      const file = e.target.files[0];
+      const imgUrl = window.URL.createObjectURL(file);
+
+      img.setAttribute('src', imgUrl);
+      
+      spinner.style.display = "inline";
+      recognizeImage();
+  })
